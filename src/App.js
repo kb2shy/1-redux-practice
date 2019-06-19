9976062995b240c80992f489507e7f08a6da348f
kb2shy <chhaianpin@hotmail.com>
2019-06-18 21:58:27 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import TasksPage from './components/TasksPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  onCreateTask = ({title, description}) => {
+    this.props.dispatch({
+      type: 'CREATE_TASK',
+      payload: {title, description}
+    });
+  }
+
+  render() {
+    console.log('props from App: ', this.props)
+    return (
+      <div className="main-content">
+        <TasksPage
+          tasks={this.props.tasks}
+          onCreateTask={this.onCreateTask}
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    tasks: state.tasks
+  }
+}
+
+export default connect(mapStateToProps)(App);
